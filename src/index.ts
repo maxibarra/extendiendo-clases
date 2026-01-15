b95aca538086450778fe84ae57df480fdb978338
maxibarra <maxi.8379@gmail.com>
2026-01-14 21:06:36 -0300

    clase listaDeProductos realizada con los metodos pedidos en el desafio

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import products from "./products.json";
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -17,6 +19,7 @@ class Product {
   name: string;
   price: number;
   id: number;
+
   constructor(name: string, price: number, id: number) {
     this.name = name;
     this.price = price;
@@ -24,6 +27,42 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+
+  constructor(n:string){
+    super(n);
+
+    products.forEach(product => this.addProduct(product));
+  }
+
+  addProduct(product: Product): void{
+    //verificamos si ya existe el producto
+    const existe = this.cosas.some(p=> p.id === product.id);
+    if(!existe){
+      //sino existe lo agregamos
+      this.cosas.push(product)
+    }else{
+      console.log(`El producto con id ${product.id} ya existe`);
+    }
+  }
+  getProduct(id:number): Product{
+   return this.cosas.find(p => p.id === id);   
+  }
+
+  removeProduct(id:number): void{
+    //crea un nuevo array sin el producto que queremos eliminar
+    this.cosas = this.cosas.filter(p => p.id !== id);
+  }
+
+  getSortedByPrice(order: string): Product[]{
+    return this.cosas.sort((a,b) => {
+      if(order === "desc"){
+        return b.price - a.price; // orden descendente
+      }else{
+        return a.price - b.price; //orden ascendente
+      }
+    });
+  }
+}
 
 export { ListaDeProductos, Product };
